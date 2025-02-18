cd103ff76646c522fb6ccb96a5b37187872c30b6
RereTooki <rereloluwa.afolabi.tooki@gmail.com>
2025-02-18 18:05:39 +0100

    added a modal

--- src/components/AlertModal2.tsx
+++ src/components/AlertModal2.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
+import { Dialog } from "@headlessui/react";
 import { IoMdClose } from "react-icons/io";
 import { projectsSites } from "./Projects";
 import { SiGmail } from "react-icons/si";
@@ -43,18 +43,19 @@ const AlertModal2: React.FC<AlertModal2Props> = ({ isOpen2, onClose }) => {
         <div className="fixed inset-0 z-10 w-screen overflow-y-auto ">
           <div className="flex min-h-full items-center justify-center p-4">
             <Dialog.Title className="fixed inset-0 bg-black opacity-30" />
-            <DialogPanel
-              transition
+            <Dialog.Panel
+              data-aos="zoom-in"
+              data-aos-duration="500"
               className="w-full max-w-md rounded-xl bg-white/90 p-6 backdrop-blur-lg duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
             >
-              <DialogTitle
+              <Dialog.Title
                 as="h3"
                 className="text-bases/7 font-semibold text-2xl text-gray-700 hover:text-gray-900 sss"
               >
                 <span className="underline underline-offset-4">
                   My Projects
                 </span>{" "}
-              </DialogTitle>
+              </Dialog.Title>
               <p className="mt-8 text-sm/6 text-gray-500 hover:text-gray-700 shadow rounded-lg p-4">
                 <p className="underline underline-offset-4 decoration-dotted">
                   Some of My Works
@@ -124,7 +125,7 @@ const AlertModal2: React.FC<AlertModal2Props> = ({ isOpen2, onClose }) => {
               <button className="absolute top-4 right-4 " onClick={onClose}>
                 <IoMdClose className="h-5 w-5" color="gray-100" />
               </button>
-            </DialogPanel>
+            </Dialog.Panel>
           </div>
         </div>
       </Dialog>
